0c1091da3253e051e99e054ed8c86d1fabdc51e8
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2025-04-10 09:54:17 +0530

    Update db_handler.py

--- backend/utils/db_handler.py
+++ backend/utils/db_handler.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 def save_to_sql_server(df, table_name):
-    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=sql_server;DATABASE=datasetdb;UID=sa;PWD=your_password')
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=sql_server;DATABASE=datasetdb;UID=sa;PWD=StrongPass123!')
     cursor = conn.cursor()
     df.to_sql(table_name, con=conn, if_exists='replace', index=False)
     conn.commit()
